2d39b665ba7867ff55bef8ee1aa81929b690f8e5
Matthewinsoftware <93150923+Matthewinsoftware@users.noreply.github.com>
2022-07-29 19:44:20 -0400

    Create StoreLinks.vue

---
+++ StoreLinks.vue
@@ -0,0 +1,39 @@
+<template>
+  <h4>
+  <q-page class="q-pa-lg">
+    <center><h2 class="q-mt-none">Grocery Store Links</h2></center>
+    <ul>
+
+     <br>   <p >Link To Kroger</p>
+    <p><a href="https://www.kroger.com" title="Kroger Website">Kroger</a></p>
+
+<br>
+         <p>Link To Walmart</p>
+    <p><a href="https://www.walmart.com" title="Walmart Website">Walmart</a></p>
+
+            <br>
+          <p>Link To Trader Joes</p>
+    <p><a href="https://www.traderjoes.com" title="Trader Joes Website">Trader Joes</a></p>
+
+            <br>
+           <p>Link To Publix</p>
+    <p><a href="https://www.publix.com" title="Publix Website">Publix</a></p>
+
+            <br>
+           <p>Link To WholeFoods</p>
+    <p><a href="https://www.wholefoods.com" title="WholeFoods Website">Whole Foods</a></p>
+</ul>
+  </q-page>
+  </h4>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+
+
+</script>
+
